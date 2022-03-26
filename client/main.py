df8aea983f6e95ba79956473e7efabe3d70467d5
Arash Sameni <arashsameniy@gmail.com>
2022-03-26 22:15:54 +0430

    add command class

--- client/main.py
+++ client/main.py
@@ -3,7 +3,7 @@ from time import sleep
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 65432))
-s.sendall(b"Hello, world")
+s.sendall("HELP".encode('utf-8'))
 while True:
     data = s.recv(1024)
     print(f"Received {data!r}")
--- server/main.py
+++ server/main.py
@@ -1,9 +1,10 @@
-from ast import arg
 import socket
 import threading
 
 HOST = "127.0.0.1"
 PORT = 65432
+ENC_TYPE = 'utf-8'
+MESSAGE_SIZE = 1024
 COLORS = {
     "BLACK": "\u001b[30;1m",
     "RED": "\u001b[31;1m",
@@ -20,6 +21,27 @@ active_connections = 0
 print_lock = threading.Lock()
 
 
+class Command():
+    HELP = 'help'
+    LIST = 'ls'
+    GET = 'get'
+    PUT = 'put'
+    PWD = 'pwd'
+    MKDIR = 'mkdir'
+    CD = 'cd'
+    DELETE = 'delete'
+    RENAME = 'rename'
+
+    def __init__(self, cmd):
+        self.type, self.args = Command.split_command(cmd)
+
+    def split_command(cmd):
+        first_space = cmd.find(' ')
+        if first_space < 0:
+            return cmd, ''
+        return cmd[:first_space], cmd[first_space + 1:]
+
+
 class ClientThread(threading.Thread):
     def __init__(self, conn, addr, *args):
         self.conn = conn
@@ -27,9 +49,60 @@ class ClientThread(threading.Thread):
         threading.Thread.__init__(self, args=args)
 
     def run(self):
+        global ENC_TYPE, active_connections
+
         print_lock.acquire()
-        print("Starting " + str(self.addr))
+        print(COLORS['YELLOW'] +
+              f'[NEW CONNECTION] {self.addr[0]} connected at port {self.addr[1]}' +
+              COLORS['RESET'])
         print_lock.release()
+
+        while True:
+            message = self.conn.recv(MESSAGE_SIZE).decode(ENC_TYPE)
+            if not message:
+                break
+            print_lock.acquire()
+            print(COLORS['CYAN'] +
+                  f'[{self.addr[0]}:{self.addr[1]}] "{message}"' +
+                  COLORS['RESET'])
+            print_lock.release()
+
+            cmd = Command(message)
+            if cmd.type == Command.HELP:
+                self.help()
+            elif cmd.type == Command.LIST:
+                pass
+            elif cmd.type == Command.GET:
+                pass
+            elif cmd.type == Command.PUT:
+                pass
+            elif cmd.type == Command.PWD:
+                pass
+            elif cmd.type == Command.MKDIR:
+                pass
+            elif cmd.type == Command.CD:
+                pass
+            elif cmd.type == Command.DELETE:
+                pass
+            elif cmd.type == Command.RENAME:
+                pass
+
+        self.close_connection()
+
+    def close_connection(self):
+        active_connections -= 1
+        print_lock.acquire()
+        print(COLORS['RED'] +
+              f'[CONNECTION LOST] {self.addr[0]}:{self.addr[1]} disconnected' +
+              COLORS['RESET'])
+        print(COLORS['GREEN'] +
+              f'[ACTIVE CONNECTIONS] {active_connections}' +
+              COLORS['RESET'])
+        print_lock.release()
+        self.conn.close()
+
+    def help(self):
+        print("requested help")
 
 
 def handle_incoming_requests(socket):
@@ -42,9 +115,6 @@ def handle_incoming_requests(socket):
         active_connections += 1
 
         print_lock.acquire()
-        print(COLORS['YELLOW'] +
-              f'[NEW CONNECTION] {addr[0]} connected at port {addr[1]}' +
-              COLORS['RESET'])
         print(COLORS['GREEN'] +
               f'[ACTIVE CONNECTIONS] {active_connections}' +
               COLORS['RESET'])
